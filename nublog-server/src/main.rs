c419b324d24e2ecaa531028d483e74c8282fcfd1
Nugine <nugine@foxmail.com>
2020-05-29 23:41:30 +0800

    server: hello world

--- nublog-server/src/main.rs
+++ nublog-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use nuclear::core::{App, Result};
+use nuclear::web;
+
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, world!");
+
+    let addr = "127.0.0.1:8000";
+
+    let app = App::builder().endpoint(|_| async { web::reply::text("hello world") });
+
+    app.run(addr).await
 }
