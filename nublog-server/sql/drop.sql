2148047ce9a2081f4bb97684b5c54b4582840510
Nugine <nugine@foxmail.com>
2020-05-31 22:18:26 +0800

    server: add setup.sql

---
+++ nublog-server/sql/drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE users CASCADE;
+
+DROP TRIGGER articles_update_at ON articles;
+DROP FUNCTION set_update_at();
+DROP TABLE articles CASCADE;
+
+DROP TABLE tags CASCADE;
+
+DROP TABLE articles_tags_relation CASCADE;
+
+DROP TABLE comments CASCADE;
---
+++ nublog-server/sql/setup.sql
@@ -0,0 +1,68 @@
+-- create table users
+
+CREATE TABLE users(
+    id SERIAL NOT NULL PRIMARY KEY,
+    role SMALLINT NOT NULL CONSTRAINT users_role_check CHECK (role BETWEEN 0 AND 1),
+    name VARCHAR(256) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    avatar_url VARCHAR(512) NOT NULL,
+    profile_url VARCHAR(512) NOT NULL,
+    github_token VARCHAR(512)
+);
+
+----------------------------------
+
+-- create table articles
+
+CREATE TABLE articles(
+    id SERIAL NOT NULL PRIMARY KEY,
+    article_key VARCHAR(256) NOT NULL UNIQUE,
+    title VARCHAR(256) NOT NULL,
+    author VARCHAR(256) NOT NULL,
+    content TEXT NOT NULL,
+    create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION set_update_at() RETURNS TRIGGER AS $articles$
+BEGIN
+    NEW.update_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$articles$ LANGUAGE plpgsql;
+
+CREATE TRIGGER articles_update_at BEFORE UPDATE ON articles
+FOR EACH ROW
+EXECUTE PROCEDURE set_update_at();
+
+----------------------------------
+
+-- create table tags
+
+CREATE TABLE tags(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL UNIQUE
+);
+
+-- article tag n:n
+
+CREATE TABLE articles_tags_relation(
+    article_id INTEGER NOT NULL REFERENCES articles(id),
+    tag_id INTEGER NOT NULL REFERENCES tags(id),
+    PRIMARY KEY (article_id, tag_id)
+);
+
+--------------------------------
+
+-- create table comments
+
+CREATE TABLE comments(
+    id SERIAL NOT NULL PRIMARY KEY,
+    article_id INTEGER NOT NULL REFERENCES articles(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    content TEXT NOT NULL,
+    reply_to INTEGER REFERENCES comments(id),
+    create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+--------------------------------
