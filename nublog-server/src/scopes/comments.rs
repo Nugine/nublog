c1ca506e5a45864a57954f7a3dc1efcbb7fc1209
Nugine <nugine@foxmail.com>
2020-06-09 21:30:55 +0800

    server: impl CommentRepo

--- nublog-server/src/scopes/comments.rs
+++ nublog-server/src/scopes/comments.rs
@@ -9,18 +9,42 @@ pub mod entity {
         pub reply_to: Option<i32>,
         pub create_at: DateTime,
     }
+
+    pub type CommentRepo = Repo<Comment>;
+
+    impl CommentRepo {
+        pub async fn insert_comment(&self, dto: super::dto::CreateCommentReq) -> Result<i32> {
+            let mut conn: Conn = self.get_conn().await?;
+            let query = sqlx::query!(
+                "INSERT INTO comments(article_id, user_id, content, reply_to) VALUES($1, $2, $3,$4) RETURNING id",
+                dto.article_id,
+                dto.user_id,
+                dto.content,
+                dto.reply_to
+            );
+            let ans = query.fetch_one(&mut conn).await?;
+            Ok(ans.id)
+        }
+
+        pub async fn delete_comment_by_id(&self, id: i32) -> Result<bool> {
+            let mut conn: Conn = self.get_conn().await?;
+            let query = sqlx::query!("DELETE FROM comments WHERE id = $1", id);
+            let ans = query.execute(&mut conn).await?;
+            Ok(ans == 1)
+        }
+    }
 }
 
 pub mod dto {
     use crate::prelude::*;
-    
+
     #[derive(Debug, Serialize, Deserialize)]
     // 创建评论
     pub struct CreateCommentReq {
-        article_id: u32,
-        user_id: u32,
-        content: String,
-        reply_to: Option<u32>,
+        pub article_id: i32,
+        pub user_id: i32,
+        pub content: String,
+        pub reply_to: Option<i32>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
