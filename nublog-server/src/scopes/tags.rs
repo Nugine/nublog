1b0eaac05ed3cb7cbe8f4cbc587f6f3a77c89158
Nugine <nugine@foxmail.com>
2020-06-09 21:37:43 +0800

    server: impl TagRepo

--- nublog-server/src/scopes/tags.rs
+++ nublog-server/src/scopes/tags.rs
@@ -1,7 +1,36 @@
 pub mod entity {
+    use crate::create;
+    use crate::prelude::*;
+
     pub struct Tag {
         id: i32,
         name: String,
+    }
+
+    pub type TagRepo = Repo<Tag>;
+
+    impl TagRepo {
+        pub async fn select_all_tags(&self) -> Result<Vec<Tag>> {
+            let mut conn: Conn = self.get_conn().await?;
+            let query = sqlx::query!("SELECT * FROM tags");
+            let anss = query.fetch_all(&mut conn).await?;
+            let tags = anss
+                .into_iter()
+                .map(|ans| create!(Tag from ans by id, name))
+                .collect();
+            Ok(tags)
+        }
+
+        pub async fn select_tag_articles(&self, id: i32) -> Result<Vec<i32>> {
+            let mut conn: Conn = self.get_conn().await?;
+            let query = sqlx::query!(
+                "SELECT article_id FROM articles_tags_relation WHERE tag_id = $1",
+                id
+            );
+            let anss = query.fetch_all(&mut conn).await?;
+            let article_ids = anss.into_iter().map(|ans| ans.article_id).collect();
+            Ok(article_ids)
+        }
     }
 }
 
