88ffc9d340e96befe69443e08bd3f30050f9e5b3
Nugine <nugine@foxmail.com>
2020-06-15 13:30:20 +0800

    server: change session

--- nublog-server/src/session.rs
+++ nublog-server/src/session.rs
@@ -1,8 +1,12 @@
-use nuclear::core::{InjectorExt, Request, Result};
+use crate::prelude::*;
+use std::collections::HashMap;
+use std::time::SystemTime;
+use tokio::sync::RwLock;
 
+#[derive(Debug, Clone)]
 pub struct Session {
-    pub id: i32,
-    pub expire_time: u32,
+    pub id: Uuid,
+    pub expire_time: SystemTime,
     pub user_id: i32,
     pub role_code: i32,
 }
@@ -17,6 +21,40 @@ pub trait SessionExt {
 
 impl SessionExt for Request {
     fn try_get_session_ref(&self) -> Result<&Session, MissingSessionError> {
-        self.try_inject_ref().map_err(|_| MissingSessionError)
+        self.local().get().ok_or_else(|| MissingSessionError)
+    }
+}
+
+pub struct SessionStore {
+    map: RwLock<HashMap<Uuid, Session>>,
+}
+
+pub fn session_store() -> SessionStore {
+    SessionStore {
+        map: RwLock::new(HashMap::new()),
+    }
+}
+
+#[async_trait]
+impl Middleware for SessionStore {
+    async fn call(&self, mut req: Request, next: Next<'_>) -> Result<Response> {
+        let token = req
+            .headers_mut()
+            .get(http::header::HeaderName::from_static("X-SESSION-ID"));
+
+        if let Some(token) = token {
+            let token = token.to_str()?;
+            let sess_id: Uuid = token.parse()?;
+            if let Some(sess) = self.map.read().await.get(&sess_id) {
+                req.local_mut().insert(sess.clone());
+            }
+        }
+
+        let res: Response = next.call(req).await?;
+        if let Some(sess) = res.local().get::<Session>() {
+            self.map.write().await.insert(sess.id.clone(), sess.clone());
+        }
+
+        Ok(res)
     }
 }
