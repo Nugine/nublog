36ff6f4aa0fb319ed3c3aa48c6324c6f2ddc740a
Nugine <nugine@foxmail.com>
2020-06-15 13:31:14 +0800

    server: update prelude

--- nublog-server/src/prelude.rs
+++ nublog-server/src/prelude.rs
@@ -5,9 +5,14 @@ pub type DateTime = chrono::DateTime<chrono::Utc>;
 pub use serde::{Deserialize, Serialize};
 
 pub use crate::conn::{Conn, PgConnExt};
-pub use crate::session::SessionExt;
+pub use crate::session::{Session, SessionExt};
 
-pub use nuclear::core::{async_trait, InjectorExt, Middleware, Next, Request, Response, Result};
+pub use nuclear::core::{
+    async_trait, InjectorExt, LocalExt, Middleware, Next, Request, Response, Result,
+};
+pub use nuclear::http;
 pub use nuclear::web::body::JsonExt;
 pub use nuclear::web::reply::{self, Json};
-pub use nuclear::web::router::SimpleRouterExt;
+pub use nuclear::web::router::{SimpleRouterExt,SimpleRouter};
+
+pub use uuid::Uuid;
