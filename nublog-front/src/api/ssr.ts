f1ff739928877f29bac270ba189627ee54e4bcdb
Nugine <nugine@foxmail.com>
2020-07-08 11:36:50 +0800

    front: pages: add /articles/[key]

--- nublog-front/src/api/ssr.ts
+++ nublog-front/src/api/ssr.ts
@@ -1,16 +1,21 @@
-import axios from "axios";
-
 import * as dto from "./dto";
 import * as config from "../config";
 
+import axios, { AxiosRequestConfig } from "axios";
+
 const SSR_URL_PREFIX = config.api.ssrUrlPrefix;
 
-async function ssrGet(url: string): Promise<unknown> {
+async function ssrGet(url: string, config?: AxiosRequestConfig): Promise<unknown> {
     const fullUrl = SSR_URL_PREFIX + url;
-    const res = await axios.get(fullUrl);
+    const res = await axios.get(fullUrl, config);
     return res.data;
 }
 
 export async function getAllArticles(): Promise<dto.QueryArticleRes[]> {
     return await ssrGet("/articles/query_all") as dto.QueryArticleRes[];
+}
+
+export async function getArticleByKey(key: string): Promise<dto.QueryArticleRes> {
+    const config = { params: { "url_key": key } };
+    return await ssrGet("/articles/query_by_key", config) as dto.QueryArticleRes;
 }
---
+++ nublog-front/src/pages/articles/[key].tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { GetServerSideProps } from "next";
+import { Row, Layout, Col } from "antd";
+
+import Error404 from "../404";
+import * as vo from "../../vo";
+import * as ssr from "../../api/ssr";
+
+export type ArticleProps = {
+    article?: vo.Article;
+}
+
+export const getServerSideProps: GetServerSideProps<ArticleProps> = async (ctx) => {
+    const urlKey = ctx.params?.key;
+    if (typeof urlKey !== "string") {
+        throw new Error("unexpected type");
+    }
+
+    try {
+        const article = await ssr.getArticleByKey(urlKey);
+        return { props: { article } };
+    } catch (e) {
+        if (e.response.status === 404) {
+            return { props: {} };
+        }
+        throw e;
+    }
+};
+
+const Article: React.FC<ArticleProps> = ({ article }: ArticleProps) => {
+    if (article) {
+        return (
+            <Layout style={{ padding: "0 1em", marginTop: "1em", backgroundColor: "white", flexGrow: 1 }}>
+                <Layout.Content style={{ flexGrow: 1 }}>
+                    <Row justify="center">
+                        <Col span={24} lg={16}>
+                            <h1 style={{ width: "100%", textAlign: "center" }}>{article.title}</h1>
+                            <Row
+                                justify="space-between"
+                                style={{ borderBottom: "1px solid #eaeaea", marginBottom: "1em" }}
+                            >
+                                <span>作者：{article.author}</span>
+                                <span>时间：{vo.fmtTime(new Date(article.updated_at), true)}</span>
+                            </Row>
+                            {article.content} {/*TODO: markdown*/}
+                        </Col>
+                    </Row>
+                </Layout.Content>
+            </Layout>
+        );
+    } else {
+        return <Error404 />;
+    }
+};
+
+export default Article;
