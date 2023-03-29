59faf70b8df4c4687f62af93f661e91607190c66
Nugine <nugine@foxmail.com>
2023-03-29 21:50:20 +0800

    vite-plugin-nuxt-content

--- modules/content/vite.ts
+++ modules/content/vite.ts
@@ -1,4 +1,3 @@
-import { useLogger } from "@nuxt/kit";
 import { PluginOption } from "vite";
 import { MarkdownRegistry } from "./markdown";
 import { stripPrefix, stripSuffix } from "./utils";
@@ -6,8 +5,6 @@ import { stripPrefix, stripSuffix } from "./utils";
 export interface Options {
     registry: MarkdownRegistry;
 }
-
-const logger = () => useLogger("vite-plugin-nuxt-content");
 
 function matchFile(file: string, contentDir: string): string | null {
     const fullPath = stripSuffix(file, "?macro=true") ?? file;
@@ -25,23 +22,12 @@ export default ({ registry }: Options): PluginOption => ({
         const filePath = matchFile(file, registry.contentDir);
         if (!filePath) return;
 
+        // https://github.com/nuxt/nuxt/blob/main/packages/nuxt/src/pages/page-meta.ts
+        if (file.endsWith("?macro=true")) {
+            return { code: "<template></template>", map: null };
+        }
+
         const output = await registry.compile(filePath, content);
-        return output.vue;
-    },
-
-    async handleHotUpdate(ctx) {
-        const filePath = matchFile(ctx.file, registry.contentDir);
-        if (!filePath) return;
-
-        const read = ctx.read;
-        ctx.read = async () => {
-            const content = await read();
-            const output = await registry.compile(filePath, content);
-
-            const consola = logger();
-            consola.info(`hot update: ${filePath}`);
-
-            return output.vue;
-        };
+        return { code: output.vue, map: null };
     },
 });
