b212518f78fa1a83ca34a5e1c86f7a934a616ede
Nugine <nugine@foxmail.com>
2023-04-02 20:58:29 +0800

    components: MarkdownArea

--- components/MarkdownPage.vue
+++ components/MarkdownPage.vue
@@ -1,24 +1,17 @@
 <template>
-    <div class="markdown-area">
+    <MarkdownArea>
         <slot />
-    </div>
+    </MarkdownArea>
 </template>
 
-<style></style>
-
 <script setup lang="ts">
-import { MarkdownData } from "~~/modules/content/markdown";
+import MarkdownArea from "./markdown/MarkdownArea.vue";
+import { MarkdownData } from "~/modules/content/markdown";
 import { useAppConfig, useHead } from "#imports";
 
-import "~/assets/markdown.css";
-import "katex/dist/katex.min.css";
-
-const props = defineProps<{
-    meta: MarkdownData["meta"];
-}>();
+const props = defineProps<{ meta: MarkdownData["meta"] }>();
 
 const config = useAppConfig();
-
 const pageTitle = `${props.meta.title} | ${config.siteTitle}`;
 useHead({ title: pageTitle });
 </script>
---
+++ components/markdown/MarkdownArea.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="markdown-area">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import "~/assets/markdown.css";
+import "katex/dist/katex.min.css";
+</script>
