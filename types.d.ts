e9dec09fd751f183daedc689a32a06c884bbedb1
Nugine <nugine@foxmail.com>
2023-08-26 13:46:16 +0800

    nuxt upgrade

--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,15 @@
 declare module "@hpcc-js/wasm/graphviz" {
     export * from "@hpcc-js/wasm/types/graphviz";
 }
+
+declare module "*.md" {
+    import { type DefineComponent } from "vue";
+    const component: DefineComponent<unknown, unknown, unknown>; // TODO: what should be the type here?
+    export default component;
+}
+
+declare module "virtual:nuxt-content-index" {
+    import type { MarkdownMeta } from "./markdown";
+    const _default: MarkdownMeta[];
+    export default _default;
+}
