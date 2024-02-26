0cc015202f3f1345cfc92c62b144db425a2bb2ed
Nugine <nugine@foxmail.com>
2024-02-26 21:15:00 +0800

    nuxt upgrade

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,3 @@
-// https://github.com/nuxt/nuxt/issues/19606
-// https://github.com/hpcc-systems/hpcc-js-wasm#-typescript-notes
-declare module "@hpcc-js/wasm/graphviz" {
-    export * from "@hpcc-js/wasm/types/graphviz";
-}
-
 declare module "*.md" {
     import { type DefineComponent } from "vue";
     const component: DefineComponent<unknown, unknown, unknown>; // TODO: what should be the type here?
